416517cdb12698dc6d210c8a23578b16e818ae88
dominicherrmann <dherrmann.oas@gmail.com>
2023-02-26 22:00:14 +0100

    Removed unnecessary components

--- algorithmVisualizer.py
+++ algorithmVisualizer.py
@@ -1,65 +1,30 @@
 import tkinter as tk
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
-class AlgorithmVisualizer(tk.Frame):
+class AlgorithmVisualizer:
     
-    def __init__(self, *args):
-        tk.Frame.__init__(self, *args)
-        # heading = tk.Label(self, text="Select the kind of Algorithm you like to visualize:", font=("Arial", 15))
-        heading = tk.Label(self, text="Sorting Algorithm Visualizer", font=("Arial", 15))
+    def __init__(self):
+        root = tk.Tk()
+        root.geometry("900x500")
+        root.title("Algorithm-Visualizer-Python")
+        root.resizable(False, False)
+
+        heading = tk.Label(root, text="Select Sorting Algorithm you like to visualize:", font=("Arial", 15))
         heading.pack(side="top", anchor="nw", padx=10, pady=10)
 
-        # Define Buttons for Sorting in frame
-        self.sortingAlgorithms = ["Bubble Sort", "Insertion Sort", "Selection Sort", "Heap Sort",
-                             "Counting Sort", "Radix Sort", "Merge Sort", "Quick Sort"]
-        
-        # Creating Container
-        buttonFrame = tk.Frame(self)
-        container = tk.Frame(self)
-        buttonFrame.pack(side="top", fill="x", expand=False)
-        container.pack(side="top", fill="both", expand=True)
-
-        # Define Frame for Algorithm Visualizers
-        bubbleSort = BubbleSort()
-        selectionSort = SelectionSort()
-        insertionSort = InsertionSort()
-
-        bubbleSort.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        selectionSort.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        insertionSort.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-
         # Creating Buttons for Algorithm Selection
-        bubbleSortButton = tk.Button(buttonFrame, text="Bubble Sort", width=50, height=5, command=lambda: bubbleSort.visualize_bubble_sort(buttonFrame))
-        selectionSortButton = tk.Button(buttonFrame, text="Selection Sort", width=50, height=5, command=lambda: selectionSort.visualize_selection_sort(buttonFrame))
-        insertionSortButton = tk.Button(buttonFrame, text="Insertion Sort", width=50, height=5, command=lambda: insertionSort.visualize_insertion_sort(buttonFrame))
+        bubbleSortButton = tk.Button(root, text="Bubble Sort", width=50, height=5, command=self.visualize_bubble_sort)
+        selectionSortButton = tk.Button(root, text="Selection Sort", width=50, height=5, command=self.visualize_selection_sort)
+        insertionSortButton = tk.Button(root, text="Insertion Sort", width=50, height=5, command=self.visualize_insertion_sort)
         
         bubbleSortButton.pack()
         selectionSortButton.pack()
         insertionSortButton.pack()
-    
-class Page(tk.Frame):
 
-    def __init__(self):
-        tk.Frame.__init__(self)
-    def show(self, buttonFrame):
-        buttonFrame.pack_forget()
-        self.lift()
+        root.mainloop()
 
-class BubbleSort(Page):
-    
-    def __init__(self):
-        Page.__init__(self)
-        # Self instead of root underneath defines that Frame
-        # instead of parent class should be used
-        label = tk.Label(self, text="This is Bubble Sort")
-        label.pack()
-
-        
-    def visualize_bubble_sort(self, buttonFrame):
-        self.show(buttonFrame)
-        print("This is bubble sort")
+    def visualize_bubble_sort(self):
 
         values = np.random.randint(0, 100, 15)
         x = np.arange(0, 15, 1) # Get values 0-14
@@ -71,18 +36,8 @@ class BubbleSort(Page):
                 plt.clf()
                 if values[j] > values[j+1]:
                     values[j], values[j+1] = values[j+1], values[j]
-
-
-class SelectionSort(Page):
-
-    def __init__(self):
-        Page.__init__(self)
-        selectionSortLabel = tk.Label(self, text="This is Selection Sort")
-        selectionSortLabel.pack()
         
-    def visualize_selection_sort(self, buttonFrame):
-        self.show(buttonFrame)
-        print("This is selection Sort")
+    def visualize_selection_sort(self):
 
         values = np.random.randint(0, 100, 15)
         x = np.arange(0, 15, 1) # Get values 0-14
@@ -101,16 +56,7 @@ class SelectionSort(Page):
             plt.pause(0.25)
             plt.clf()
 
-class InsertionSort(Page):
-
-    def __init__(self):
-        Page.__init__(self)
-        insertionSortLabel = tk.Label(self, text="This is Insertion Sort")
-        insertionSortLabel.pack()
-
-    def visualize_insertion_sort(self, buttonFrame):
-        self.show(buttonFrame)
-        print("This is insertion sort")
+    def visualize_insertion_sort(self):
 
         values = np.random.randint(0, 100, 15)
         x = np.arange(0, 15, 1) # Get values 0-14
@@ -133,12 +79,5 @@ class InsertionSort(Page):
                     
 
 if __name__ == "__main__":
-    print("Starting program")
-    root = tk.Tk()
-    root.geometry("900x500")
-    root.title("Algorithm-Visualizer-Python")
-    root.resizable(False, False)
-    main = AlgorithmVisualizer(root)
-    main.pack(side="top", fill="both", expand=True)
-    root.mainloop()
+    AlgorithmVisualizer()
 
